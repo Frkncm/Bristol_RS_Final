da2cb5c6f0ca4b98182ecc2f4ab25fc24a687e64
Frkncm <furkancam_38@hotmail.com>
2021-01-03 08:49:57 +0000

    update

--- compImpl/compImpl.ino
+++ compImpl/compImpl.ino
@@ -49,47 +49,6 @@ uint8_t countTask{ 0 };
 
 float bearing {0};
 
-Kalman kalmanX;
-Kalman kalmanY;
-
-double accX, accY, accZ;
-double gyroX, gyroY, gyroZ;
-double gyroXangle, gyroYangle; // Angle calculate using the gyro only
-double compAngleX, compAngleY; // Calculated angle using a complementary filter
-double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
-float kalmanCal{0};
-void kalmanImplementation(void) {
-  static uint32_t timer;
-  imu.read();
-  accX = imu.a.x;
-  accY = imu.a.y;
-  accZ = imu.a.z;
-
-  //gyroX = imu.g.x;
-  gyroY = imu.g.y;
-  //gyroZ = imu.g.z;
-
-  double dt = (double)(micros() - timer) / 1000000.0; // Calculate delta time
-  timer = micros();
-
-  //float roll  = atan2(accY_, accZ_) * RAD_TO_DEG;
-  //float pitch = atan(-accX_ / sqrt(accY_ * accY_ + accZ_ * accZ_)) * RAD_TO_DEG;
-  float pitch = atan (accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
-  //float roll =  atan (accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
-  //float yaw =   atan (accZ / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
-
-  //double gyroXrate = gyroX * 0.00875; // Convert to deg/s
-  double gyroYrate = gyroY * 0.00875; // Convert to deg/s
-  //double gyroZrate = gyroZ * 0.00875; // Convert to deg/s
-
-  //kalmanX.setAngle(roll);
-  //kalmanY.setAngle(pitch);
-
-  //kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
-  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
-}
-
-
 //square implementation
 bool taskHandler() {
   auto driveStraight = [](float theta_demand)->float{return H_PID.updateValue(0 ,
@@ -138,15 +97,15 @@ uint8_t loc_speed = 0;
 
 //create the function which is called periodically
 void imuTask(void);
-taskInsert imuTaskIns(imuTask, 15);
+taskInsert imuTaskIns(imuTask, 10);
 
 void imuTask(void) {
 
-  //cmp.updateFilter();
-  kalmanImplementation();
+  cmp.updateFilter();
+
   //get the coordinate using IMU sensor parameters
   imclc.turnUpdate();
-  imclc.getCoordinate(imclc.turnRotation, (kalAngleY - kalmanCal));
+  imclc.getCoordinate(imclc.turnRotation, cmp.getFilteredY());
 
   loc_speed += 3;
 
@@ -167,39 +126,6 @@ void imuTask(void) {
 
 }
 
-
-void exampleTask(void);
-taskInsert exampleTaskIns(exampleTask, 10);
-
-void exampleTask(void) {
-
-  static int staticTime = 0;
-  //update the filter values
-  cmp.updateFilter();
-  kalmanImplementation();
-  //get the coordinate using IMU sensor parameters
-  imclc.turnUpdate();
-  imclc.getCoordinate(imclc.turnRotation, kalAngleY);
-
-  staticTime++;
-  if (staticTime > 200 && staticTime < 300) {
-    leftMotor.motorControl(-_SPEED_ + H_PID.updateValue(0 , (atan((0 - imclc.turnRotation) * PI / 180.0))));
-    rightMotor.motorControl(-_SPEED_ - H_PID.updateValue(0 , (atan((0 - imclc.turnRotation) * PI / 180.0))));
-    bthc05.print(" V: "); bthc05.print(imclc.velocity);
-    bthc05.print(" D: "); bthc05.print(imclc.distance);
-    bthc05.print(" K: "); bthc05.print(kalAngleY);
-  } else {
-    leftMotor.motorControl(0);
-    rightMotor.motorControl(0);
-    imclc.velocity = 0;
-  }
-
-  bool readPin = digitalRead(30);
-  if (readPin == LOW) {
-    staticTime = 0;
-  }
-}
-
 void setup() {
   // put your setup code here, to run once:
 
@@ -210,19 +136,11 @@ void setup() {
   delay(1000);
 
   //Calibrate all relevant parameters
-  //cmp.cal_compl_params(2000);
+  cmp.cal_compl_params(2000);
   imclc.turnSetup(1024);
 
-  float loc_kalCal = 0;
-  for (int i = 0; i < 1000; i++) {
-    kalmanImplementation();
-    loc_kalCal += kalAngleY;
-  }
-  kalmanCal = loc_kalCal / 1000.0;
-
-
   //set the calibration value as an offset
-  imclc.setOffset(kalmanCal);
+  imclc.setOffset(cmp.calibration_Y);
 
   bthc05.begin(9600);
   GO_HANDLE(IDLE_STATE); // start with handling IDLE state
@@ -239,11 +157,6 @@ void loop() {
         /*Start the state machine with the idle state (do nothing here)
           GO_HANDLE macro and the others are defined in the Romi.h file.
           They are used to navigate the flow of control. */
-        //cmp.updateFilter();
-        //kalmanImplementation();
-        //Serial.print(" KalY: "); Serial.println(kalAngleY-kalmanCal);
-        //Serial.print(" KalY: "); Serial.println(cmp.getFilteredY());
-        //exampleTaskIns.callMyTask();
         GO_HANDLE(PATH_TRACING);
         break;
       }
@@ -302,16 +215,6 @@ void loop() {
     case STOP_SYSTEM: {
         leftMotor.motorControl(0);
         rightMotor.motorControl(0);
-        bool readPin = digitalRead(30);
-        if (readPin == LOW) {
-          imclc.stable_velocity = false;
-          imclc.velocity = 0;
-          imclc.turnRotation = 0;
-          imclc.Xnew = 0;
-          imclc.Ynew = 0;
-          countTask = 0;
-          GO_HANDLE(IDLE_STATE);
-        }
         break;
       }
     default : {
@@ -320,6 +223,7 @@ void loop() {
       }
   }
 }
+
 
 
 
